64828fbfa53aae00c5f844c386b7e871f9a74b4b
ShiroSan <harlampevrenat@gmail.com>
2023-02-14 21:03:28 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<!-- Links -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body class="bg-dark">
+	<div class="main-layout">
+		<header class="primary-header bg-primary col-12 bg-blue text-center text-white" style="height: 100px;">
+			
+		</header>
+
+		<main class="col-10 mx-auto tr text-center">
+			<h1 class="text-white">This page doesn't have much content</h1>
+			<p class="text-white">I'd like to have more content, but just don't have anything more to say</p>
+			<p class="text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea iste temporibus quasi odio consequatur perferendis itaque asperiores tempore officiis sint vitae inventore facilis modi soluta libero, et deleniti quod earum!</p>
+			<p class="text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea iste temporibus quasi odio consequatur perferendis itaque asperiores tempore officiis sint vitae inventore facilis modi soluta libero, et deleniti quod earum!</p>
+
+			<form class="contact-form">
+				<div class="form-group">
+					<label for="name">Name
+					<input id="name" type="text"></label>
+				</div>
+
+				<div class="form-group">
+					<label for="email">Email
+					<input id="email" type="email"></label>
+				</div>
+
+				<div class="form-group">
+					<label for="fcolor">Favorite color</label>
+					<input id="fcolor" type="text">
+				</div>
+
+				<div class="form-group">
+					<label for="number">Number</label>
+					<input id="number" type="text">
+				</div>
+			</form>
+		</main>
+
+		<footer class="col-12 bg-primary text-center" style="height: 100px;">
+			<p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+		</footer>
+	</div>
+</body>
+
+</html>
